357dbec3c175fc43e1149c5a0422699c331a8072
Joseph DiGiovanni <jdigiovanni78@gmail.com>
2025-02-19 18:40:29 -0500

    Add initial packages

--- default.nix
+++ default.nix
@@ -1,3 +1,7 @@
 { pkgs ? import <nixpkgs> { } }:
 
-{ }
+{
+  adwaita-for-steam = pkgs.callPackage ./pkgs/adwaita-for-steam { };
+  mkvextract-gtk = pkgs.callPackage ./pkgs/mkvextract-gtk { };
+  plymouth-theme-neat = pkgs.callPackage ./pkgs/plymouth-theme-neat { };
+}
